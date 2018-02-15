afb4bc466c92d0121904a85bbf1577aea24c3646
Claudine Pradal <claudinecartailler@gmail.com>
2018-02-15 17:59:56 +0100

    Mister Cocktail App in place

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 require 'json'
 require 'open-uri'
 
-puts "Deleting all the ingredients \o/"
+puts "Deleting all the ingredients & cocktails\o/"
 
+Cocktail.destroy_all
 Ingredient.destroy_all
 
 url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
@@ -12,5 +13,17 @@ list = JSON.parse(user_serialized)
 list["drinks"].each do |drink|
   Ingredient.create!(name: drink["strIngredient1"])
 end
+
+url2 = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
+user_serialized2 = open(url2).read
+list2 = JSON.parse(user_serialized2)
+
+list2["drinks"].each do |drink|
+  Cocktail.create!(
+    name: drink["strDrink"],
+    picture: "https://#{drink["strDrinkThumb"]}"
+    )
+end
+
 
 puts "the end"
